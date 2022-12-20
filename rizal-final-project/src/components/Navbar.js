39ef2493db6fa500b0ab7fc3f17229ffbaf24d81
Janwry Dela Cruz <janwry.jrv@gmail.com>
2022-12-20 17:17:17 +0800

    added navbar to be redesign and add some details

--- rizal-final-project/src/components/Navbar.js
+++ rizal-final-project/src/components/Navbar.js
@@ -1,19 +1,35 @@
-import React from 'react';
-import '../styles/navbar.css'
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "../styles/navbar.css";
+import logo from '../assets/logo.png';
+function Navbar() {
+	const navRef = useRef();
 
-function Navbar(){
-    return(
-        <>
-            <nav>
-                <div className='nav-container'>
-                <ul>
-                    <li><a href='#'>Group no.</a></li>
-                    <li><a href='#'>Home</a></li>
-                    <li><a href='#'>About</a></li>
-                </ul>
-                </div>
-            </nav>
-        </>
-    )
+	const showNavbar = () => {
+		navRef.current.classList.toggle("responsive_nav");
+	};
+
+	return (
+		<header>
+			<div className="logo">
+                <img src={logo}/>
+            </div>
+			<nav ref={navRef}>
+				<a href="/#">Home</a>
+				<a href="/#">My work</a>
+				<a href="/#">Group no.</a>
+				<a href="/#">About me</a>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button className="nav-btn" onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
+	);
 }
+
 export default Navbar;
