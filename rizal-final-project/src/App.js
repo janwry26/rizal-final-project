93ac7f31d0d674ba28da39eb50208146073f350b
Janwry Dela Cruz <janwry.jrv@gmail.com>
2022-12-28 18:57:22 +0800

    added section to use animation on the project

--- rizal-final-project/src/App.js
+++ rizal-final-project/src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar'
 import Hero from './components/Hero';
-
+import Section1 from './components/Section1';
 function App() {
   return (
    <>
     <Navbar/>
     <Hero/>
+    <Section1/>
    </>
   );
 }
---
+++ rizal-final-project/src/components/Section1.js
@@ -0,0 +1,20 @@
+import '../styles/section1.css';
+import AOS from 'aos';
+import {useEffect } from 'react';
+
+function Section1(){
+    useEffect(()=>{
+        AOS.init()
+    },[]);
+    return(
+        <div className='top'>
+            <h1 className='text-center'>WELCOME TO REACT ANIMATION</h1>
+            <h1>FADE</h1>
+            <div className='animation' data-aos="fade-right" ></div>
+            <div className='animation' data-aos="fade-right"></div>
+            <div className='animation' data-aos="flip-right"></div>
+            <div className='animation' data-aos="fade-right"></div>
+        </div>
+    )
+}
+export default Section1;
