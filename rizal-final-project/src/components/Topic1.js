a1c0ef3744f6cf3ddbb7fc523acfb75b34bc1165
Janwry Dela Cruz <janwry.jrv@gmail.com>
2023-01-04 01:40:07 +0800

    Added article on topic section

--- rizal-final-project/src/components/Topic1.js
+++ rizal-final-project/src/components/Topic1.js
@@ -4,11 +4,28 @@ import exile from '../assets/hero-exile.jpg';
 import exile1 from '../assets/hero-exile1.jpg';
 import arrest from '../assets/hero-arrested.jpg';
 import arrest1 from '../assets/hero-arrested1.jpg';
+import arrive from '../assets/hero-arrive.jpg';
+
 
 function Article() {
   return (
     <article className="article">
       <h1 className="article__title text-center py-3" data-aos="fade-up">RIZAL’S EXILE IN DAPITAN </h1>
+    <h1 className='text-center title'  
+        data-aos="fade-left"    
+        data-aos-duration="1500"
+        data-aos-offset="200"
+        >INTRODUCTION</h1>
+      <div className="article__content"  
+        data-aos="fade-up"
+        data-aos-duration="1500"
+        data-aos-offset="200"
+        >
+        <p>
+            The arrival of Jose Rizal in Manila on June 26, 1892 caused a great deal of excitement among the people of the Philippines. Because of his popularity, the Spaniards were afraid of him, and as a result, they watched his every move very carefully. All of the homes where he had been searched, and any Filipinos who were seen in his company, were considered to be suspects. In accordance with his plans, he established the La Liga Filipina on July 3, 1892. The event took place in the home of Doroteo Ongjunco in Tondo, Manila. 
+        </p>
+       
+      </div>
     <div className='hero-container'>
       <img
         src={exile}
@@ -28,11 +45,13 @@ function Article() {
         />  
       
     </div>
+      {/* section2 */}
+     
     <h1 className='text-center title'  
         data-aos="fade-left"    
         data-aos-duration="1500"
         data-aos-offset="200"
-        >INTRODUCTION</h1>
+        >WHY RIZAL ARRESTED</h1>
       <div className="article__content"  
         data-aos="fade-up"
         data-aos-duration="1500"
@@ -43,8 +62,6 @@ function Article() {
         </p>
        
       </div>
-      {/* section2 */}
-     
     <div className='hero-container'>
       <img
         src={arrest}
@@ -68,7 +85,7 @@ function Article() {
         data-aos="fade-left"    
         data-aos-duration="1500"
         data-aos-offset="200"
-        >WHY RIZAL ARRESTED</h1>
+        >ARRIVAL IN DAPITAN</h1>
       <div className="article__content"  
         data-aos="fade-up"
         data-aos-duration="1500"
@@ -79,6 +96,25 @@ function Article() {
         </p>
        
       </div>
+    <div className='hero-container'>
+      <img
+        src={arrive}
+        alt="Hero image"
+        className="article__hero-image img-fluid img-thumbnail"
+        data-aos="fade-left"
+        data-aos-duration="1500"
+        data-aos-offset="200"
+        />
+        <img
+        src={arrive}
+        alt="Hero image"
+        className="article__hero-image img-fluid img-thumbnail"
+        data-aos="fade-right"
+        data-aos-duration="1500"
+        data-aos-offset="200"
+        />  
+      
+    </div>
     </article>
   );
 }
