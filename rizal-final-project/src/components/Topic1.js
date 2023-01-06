c39fcb06a64c4df338b4a21468f2579e0f0bd37f
Janwry Dela Cruz <janwry.jrv@gmail.com>
2023-01-06 18:06:49 +0800

    finished the article section

--- rizal-final-project/src/components/Topic1.js
+++ rizal-final-project/src/components/Topic1.js
@@ -27,6 +27,15 @@ import hero from '../assets/carousel-hero.png';
 import hero1 from '../assets/carousel-hero1.jpg';
 import hero2 from '../assets/carousel-hero2.jpg';
 import hero3 from '../assets/carousel-hero3.jpg';
+import hero4 from '../assets/carousel-hero4.jpeg';
+import hero5 from '../assets/carousel-hero5.jpg';
+import hero6 from '../assets/carousel-hero6.jpg';
+import hero7 from '../assets/carousel-hero7.jpg';
+import hero8 from '../assets/carousel-hero8.jpg';
+import last from '../assets/hero-last.jpg';
+import last1 from '../assets/hero-last1.jpg';
+
+
 
 
 
@@ -503,9 +512,9 @@ function Article() {
         </p>
 
       </div>
-      <div id="carouselExampleCaptions" class="carousel slide carousel-fade" data-bs-ride="carousel"  data-aos="flip-up"
+      <div id="carouselExampleCaptions" class="carousel slide carousel-dark carousel-fade" data-bs-ride="carousel"  data-aos="fade-up"
         data-aos-duration="1500"
-        data-aos-offset="200">
+        >
         <div class="carousel-indicators">
           <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
           <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -543,6 +552,36 @@ function Article() {
               <h1>APOLINARIO MABINI</h1>
             </div>
           </div>
+          <div class="carousel-item">
+            <img src={hero4} class="d-block w-100 img-fluid img-thumbnail" alt="..."/>
+            <div class="carousel-caption d-none d-md-block ">
+              <h1>MARCELO H. DEL PILAR</h1>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src={hero5} class="d-block w-100 img-fluid img-thumbnail" alt="..."/>
+            <div class="carousel-caption d-none d-md-block ">
+              <h1>SULTAN DIPATUAN KUDARAT</h1>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src={hero6} class="d-block w-100 img-fluid img-thumbnail" alt="..."/>
+            <div class="carousel-caption d-none d-md-block ">
+              <h1>JUAN LUNA</h1>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src={hero7} class="d-block w-100 img-fluid img-thumbnail" alt="..."/>
+            <div class="carousel-caption d-none d-md-block ">
+              <h1>MELCHORA AQUINO</h1>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src={hero8} class="d-block w-100 img-fluid img-thumbnail" alt="..."/>
+            <div class="carousel-caption d-none d-md-block ">
+              <h1>GABRIELA SILANG</h1>
+            </div>
+          </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -553,6 +592,80 @@ function Article() {
           <span class="visually-hidden">Next</span>
         </button>
       </div>
+      <h1 className='text-center title'  
+        data-aos="fade-left"    
+        data-aos-duration="1500"
+        data-aos-offset="200"
+        >ISSUES AND CONTROVERSIES ON THE HEROISM OF DR. JOSE RIZAL </h1>
+      <div className="article__content"  
+        data-aos="fade-up"
+        data-aos-duration="1500"
+        data-aos-offset="200"
+        >
+       <h3 className='title'>RETRACTION CONTROVERSY </h3>
+
+        <p>
+          Four documents were discovered that contained Jose Rizal's retraction a day before his execution. Jose Rizal's rejection of masonry is stated in the retraction, as is his repudiation of his words, writings, publication, and conduct that were against the Catholic Church. Jose Rizal's alleged retraction was a requirement for him and Josephine Bracken to marry. <br/> <br/>
+          Scholars who question the existence and authenticity of Jose Rizal's alleged retraction argue that it is a forgery based on handwriting analysis, the existence of multiple versions of the document, the alleged confession of the forger, and discrepancies in the contents of the various versions of the document. They also questioned the document's existence, pointing out that no member of Rizal's family had seen it, that it did not save Rizal from execution, and that there is no public record of Rizal and Bracken's marriage.
+
+        </p>
+        <h3 className='title'>AMERICAN SPONSORSHIP </h3>
+
+        <p>
+        Renato Constantino argued in his seminal work, Veneration Without Understanding, that Rizal's preeminence among our heroes was partly due to American sponsorship, citing laws enacted by Americans that ‘reduced' the role of other historical figures in our history in order to emphasize the role of Jose Rizal. He contended that the Americans elevated Jose Rizal above other heroes by enacting laws such as Act No. 137, which established the Rizal province, Act No. 243, which authorized the construction of the Rizal Monument in Luneta, and Act No. 346, which established December 30 as Rizal Day. He also claimed that Rizal was chosen because his image of peace and compromise, as well as the perception that he never advocated for independence, suited their (American) imperialistic interests in the Philippines.  <br/> <br/>
+        On the other side of the debate, Professor Ambeth Ocampo, a known Rizal 'expert,' refuted Constantino's claims, claiming that Jose Rizal was already venerated long before the Americans sponsored him, citing President Emilio Aguinaldo's December 20, 1898 proclamation designating December 30 as a National Day of Mourning in honor of Jose Rizal. He claimed that "the Americans simply built on the people's prevailing sentiment" toward Rizal. They used Rizal's anti-Spanish, assimilationist, reformist, and peace-loving stance to advance their colonial/imperialistic agenda. 
+        </p>
+        <h3 className='title'>WAS RIZAL ANTI-REVOLUTION? </h3>
+
+        <p>
+        According to historian Renato Constantino, Rizal was an anti-revolutionary who betrayed the Philippines on two occasions: when he volunteered to serve as a doctor for the Spanish armies in the Cuban revolution and when he issued his December 15, 1898 letter in which he not only denied his participation in the revolution but also condemned it as disastrous and a waste of lives. Other historians and scholars point to Rizal's assimilate stance as evidence of his anti-revolutionary beliefs.  <br/> <br/>
+        Professor Floro Quibuyen argued in his book A Nation Aborted: Rizal, American Hegemony, and Philippine Nationalism that Rizal was a revolutionary. He cited several pieces of evidence, including Rizal's letters explaining his ideas about the revolution and Dr. Pio Valenzuela's most recent testimony. He also provided a different reading and interpretation of Jose Rizal's Dec 15 manifesto, noting that he did not oppose the revolution but saw it as premature and lacking proper preparations. He contended that in Rizal's final poem, he praised revolutionaries, and that this poem became an inspiration for revolutionaries. He also contended that Jose Rizal could not be anti-revolutionary because many of his family members were members of the Katipunan and held important positions in it.
+        </p>
+        <h3 className='title'>WAS RIZAL AN AMBIVALENT HERO OR FENCE SITTER?</h3>
+
+          <p>
+          Four contradictory testimonies of Pio Valenzuela (2 in 1896 while a prisoner in Fort Santiago) and in 1914 and 1917 contributed to the perception of Jose Rizal as an ambivalent hero. These testimonies all revolve around his meeting with Jose Rizal in Dapitan. The first two testimonies state that Rizal completely rejected the revolution, whereas the other two claim that Rizal did not completely reject the revolution and had given instructions to the Katipunan to ensure the rebellion's success.  <br/> <br/>
+          Some historians argue that Rizal was ambivalent, citing works in which he rejected armed struggle as a means of social transformation and others in which he expresses his support for the revolution. Some historians, on the other hand, argued that in order to determine whether or not Rizal was ambivalent, Jose Rizal must be read in the proper context and perspective. They also contended that Jose Rizal was a product of his time and society, and thus Rizal must be read in the context of 19th-century Philippines and from the perspective of Filipinos at the time.
+          </p>
+          <h3 className='title'>IS RIZAL STILL RELEVANT?</h3>
+
+            <p>
+              <ul>
+                <li>
+                Jose Rizal is still relevant because the social problems or "ills" that he exposed, criticized, and attempted to solve persist. Corruption, injustice, unqualified and/or incompetent government officials, and poverty are all persistent problems in the Philippines.
+                </li>
+                <li>
+                Jose Rizal's characteristics are admirable. He serves as a role model and inspiration for Filipino youth to love and serve their country and fellow citizens.
+                </li>
+                <li>
+                Studying Jose Rizal and the history of the country makes us aware of the ideals upon which our country was founded. Studying our forefathers' sacrifices for our country's freedom and sovereignty fosters nationalism and pride in our national identity.
+                </li>
+                <li>
+                Jose Rizal will always play an important role in the evolution of Philippine nationalism.
+                </li>
+              </ul>
+            </p>
+       
+      </div>
+    <div className='hero-container'>
+      <img
+        src={last}
+        alt="Hero image"
+        className="article__hero-image img-fluid img-thumbnail"
+        data-aos="fade-left"
+        data-aos-duration="1500"
+        data-aos-offset="200"
+        />
+        <img
+        src={last1}
+        alt="Hero image"
+        className="article__hero-image img-fluid img-thumbnail"
+        data-aos="fade-right"
+        data-aos-duration="1500"
+        data-aos-offset="200"
+        />  
+      
+    </div>
     </article>
   );
 }
